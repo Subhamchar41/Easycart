6ecf1bb48a12daa6ddfb2467bd6d9375007f36f4
Subham Char <subhamchar41@gmail.com>
2020-08-26 16:34:04 +0530

    ECOMMERCE WEBSITE

---
+++ EASYCART_WEBSITE/FRONT-END/src/data.js
@@ -0,0 +1,64 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 100,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: '2',
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/d2.jpg',
+      price: 530,
+      brand: ' Nike',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d3.jpg',
+      price: 370,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8
+    }, 
+    {
+      _id: '4',
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/p1.jpg',
+      price: 200,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8
+    },
+    {
+      _id: '5',
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/p2.jpg',
+      price: 560,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8
+    },
+    {
+      _id: '6',
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 630,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8
+    }
+  ]
+}
